74d9cf6e015f7e2e7474a726dde60dcbf08b92fd
Denneil-Brooking <brookd@g.toiohomai.ac.nz>
2018-03-08 16:34:51 +1300

    done

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Press <Enter> to quit the program");
+            Console.ReadKey();
+            //done//
+
+        }
+    }
+}
